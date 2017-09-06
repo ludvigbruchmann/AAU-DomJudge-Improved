89b3351abd9013d491ae820b134adb63900cef50
Ludvig Alexander Bruchmann <info@ludvig.xyz>
2017-09-06 21:57:50 +0200

    Scrape limit and completion rate

--- scrape.py
+++ scrape.py
@@ -6,100 +6,114 @@ import BeautifulSoup as soup # HTML parser
 import time                  # Needed to add last updated time stamp
 import json                  # Needed to encode the output
 
-pointInflation = 256000
+pointInflation = 256000   # Use this to adjust for time based Scoreboard
+cacheFile  = "cache.json" # Where the scraped JSON data is written
 
-# Ignore SSL error on the DomJudge website
+# Check when cache was last updated
 
-cert                = ssl.create_default_context()
-cert.check_hostname = False
-cert.verify_mode    = ssl.CERT_NONE
+checkCache   = open(cacheFile, "r")
+last_updated = json.loads(checkCache.read())["last_updated"]
+checkCache.close()
 
-domjudge = "http://localhost/Scoreboard.html" #"https://domjudge.es.aau.dk/public/"
-html     = soup.BeautifulSoup(urllib2.urlopen(domjudge, context=cert).read())
+if time.time() - last_updated > 60:
+    # Ignore SSL error on the DomJudge website
 
-output   = {                      # JSON data generated from table
-    "last_updated" : time.time(),
-    "users" : [],                 # Empty, users are added later
-    "problems" : []               # Empty, problems are added later
-}
+    cert                = ssl.create_default_context()
+    cert.check_hostname = False
+    cert.verify_mode    = ssl.CERT_NONE
 
-table        = html.find("table", { "class" : "scoreboard" })
-tableHeaders = table.findAll("th")
-problems     = []
-for problem in tableHeaders[3:]:
-    problems.append({
-        "name" : problem.find("a").text,
-        "points" : int(problem.find("span").text.replace("[","").replace(" point]","").replace(" points]",""))
-    })
-output["problems"] = problems
+    domjudge = "http://localhost/Scoreboard.html" #"https://domjudge.es.aau.dk/public/"
+    html     = soup.BeautifulSoup(urllib2.urlopen(domjudge, context=cert).read())
 
-tableBody = table.find("tbody")
-rows      = tableBody.findAll("tr")
+    output   = {                      # JSON data generated from table
+        "last_updated" : time.time(),
+        "users" : [],                 # Empty, users are added later
+        "problems" : []               # Empty, problems are added later
+    }
 
-for row in rows:
+    table        = html.find("table", { "class" : "scoreboard" })
+    tableHeaders = table.findAll("th")
+    problems     = []
+    for problem in tableHeaders[3:]:
+        problems.append({
+            "name" : problem.find("a").text,
+            "points" : int(problem.find("span").text.replace("[","").replace(" point]","").replace(" points]","")),
+            "completed" : 0 # Users who completed this problem
+        })
+    output["problems"] = problems
 
-    ID = row["id"].replace("team:", "")
+    tableBody = table.find("tbody")
+    rows      = tableBody.findAll("tr")
 
-    columns  = row.findAll("td")
-    answers = []
-    score = 0
-    completed = 0
-    attempts = 0
-    i = 0
+    for row in rows:
 
-    for problem in columns[5:]: # For every problem, [5:] because the problems start at column 6
+        ID = row["id"].replace("team:", "")
 
-        answer = {
-            "id" : i,
-            "tries" : problem.text,
-            "time" : 0,
-            "first" : False,
-            "state" : problem["class"].replace("score_", "")
-        }
+        columns  = row.findAll("td")
+        answers = []
+        score = 0
+        completed = 0
+        attempts = 0
+        i = 0
 
-        if answer["state"] == "correct":
-            answer["tries"] = str(problem.text).split("/")[0]
-            answer["time"] = int(str(problem.text).split("/")[1])
-            score += (problems[answer["id"]]["points"] / (answer["time"] / pointInflation))
-            completed += 1
+        for problem in columns[5:]: # For every problem, [5:] because the problems start at column 6
 
-        elif answer["state"] == "correct first":
-            answer["tries"] = str(problem.text).split("/")[0]
-            answer["time"] = int(str(problem.text).split("/")[1])
-            answer["state"] = answer["state"].replace(" first", "")
-            answer["first"] = True
-            score += (problems[answer["id"]]["points"] / (answer["time"] / pointInflation))
-            completed += 1
+            answer = {
+                "id" : i,
+                "tries" : problem.text,
+                "time" : 0,
+                "first" : False,
+                "state" : problem["class"].replace("score_", "")
+            }
 
-        answer["tries"] = int(answer["tries"])
+            if answer["state"] == "correct":
+                answer["tries"] = str(problem.text).split("/")[0]
+                answer["time"] = int(str(problem.text).split("/")[1])
+                score += (problems[answer["id"]]["points"] / (answer["time"] / pointInflation))
+                completed += 1
+                problems[answer["id"]]["completed"] += 1
 
-        attempts += answer["tries"]
+            elif answer["state"] == "correct first":
+                answer["tries"] = str(problem.text).split("/")[0]
+                answer["time"] = int(str(problem.text).split("/")[1])
+                answer["state"] = answer["state"].replace(" first", "")
+                answer["first"] = True
+                score += (problems[answer["id"]]["points"] / (answer["time"] / pointInflation))
+                completed += 1
+                problems[answer["id"]]["completed"] += 1
 
-        answers.append(answer)
-        i += 1
+            answer["tries"] = int(answer["tries"])
 
-    university = columns[2].find("span").text
-    name = columns[2].text.replace(university, "")
+            attempts += answer["tries"]
 
-    if attempts > 0:
-        namethislater = completed / attempts
-    else:
-        namethislater = 0
+            answers.append(answer)
+            i += 1
 
-    output["users"].append({
-        "id" : ID,
-        "name" : name,
-        "problems" : answers,
-        "score" : score,
-        "accuracy" : namethislater,
-        "completed" : completed
-    })
+        university = columns[2].find("span").text
+        name = columns[2].text.replace(university, "")
+
+        if attempts > 0:
+            namethislater = completed / attempts
+        else:
+            namethislater = 0
+
+        output["users"].append({
+            "id" : ID,
+            "name" : name,
+            "problems" : answers,
+            "score" : score,
+            "accuracy" : namethislater,
+            "completed" : completed
+        })
 
 
 
-# Output file
+    # Output file
 
-outputFileLocation = "cache.json"
-outputFile         = open(outputFileLocation, "w")
-outputFile.write(json.dumps(output))
-outputFile.close()
+    outputFile = open(cacheFile, "w")
+    outputFile.write(json.dumps(output))
+    outputFile.close()
+
+returnFile = open(cacheFile, "r")
+#print returnFile.read()
+returnFile.close()
