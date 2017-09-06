657004d69bfa6330a1fcdce72a2f94ba8e5bc8a7
Ludvig Alexander Brüchmann <info@ludvig.xyz>
2017-09-06 14:50:10 +0200

    Changed DomJudge points to completion

--- js/dji.js
+++ js/dji.js
@@ -1,6 +1,7 @@
 const domjudgeSite  = "get.php";
 const scoreboardUrl = domjudgeSite + "?data=scoreboard";
 const teamsUrl      = domjudgeSite + "?data=teams";
+const problemsUrl   = domjudgeSite + "?data=problems";
 
 function findUser(array, id) {
   for (var i = 0; i < array.length; i++) {
@@ -31,7 +32,7 @@ function advancedScore(user) {
         succeses += 1;
       }
     }
-    return [score, (succeses / attempts)];
+    return [score, succeses, attempts];
   }
   else {
     return 0;
@@ -40,30 +41,33 @@ function advancedScore(user) {
 
 $.get( scoreboardUrl, function( data ) {
   scoreboard = JSON.parse(data);
-  $.get( teamsUrl, function( data ) {
-    teams = JSON.parse(data);
-    teams.sort(function(a, b) {
-      return parseFloat(a.id) - parseFloat(b.id);
-    });
-    $('.table').html(
-      "<table id=\"users\" style=\"width: 100%\" class=\"DomJudgeTable\">" +
-      "<tr><th>#</th><th>User</th><th>Score</th><th>Accuracy</th><th>DomJudge Points</th></tr>" +
-      "</table>"
-    );
-    for (var i = 0; i < scoreboard.length; i++) {
-      user = findUser(teams, scoreboard[i]["team"]);
-      if(scoreboard[i]["score"]["num_solved"] > 0) {
-        advScore = advancedScore(scoreboard[i]);
-        $('#users').append(
-          "<tr>" +
-          "<td class=\"rank\">" + (i + 1) + "." + "</td>" +
-          "<td class=\"user\">" + user["name"] + "</td>" +
-          "<td class=\"score\">" + Math.round(advScore[0]) + "</td>" +
-          "<td class=\"accuracy\">" + Math.round(advScore[1] * 100) + "%</td>" +
-          "<td class=\"user\">" + scoreboard[i]["score"]["num_solved"] + "</td>" +
-          "</tr>"
+  $.get( problemsUrl, function ( data ) {
+    problems = JSON.parse(data);
+      $.get( teamsUrl, function( data ) {
+        teams = JSON.parse(data);
+        teams.sort(function(a, b) {
+          return parseFloat(a.id) - parseFloat(b.id);
+        });
+        $('.table').html(
+          "<table id=\"users\" style=\"width: 100%\" class=\"DomJudgeTable\">" +
+          "<tr><th>#</th><th>User</th><th>Score</th><th>Accuracy</th><th>Completion</th></tr>" +
+          "</table>"
         );
-      }
-    }
+        for (var i = 0; i < scoreboard.length; i++) {
+          user = findUser(teams, scoreboard[i]["team"]);
+          if(scoreboard[i]["score"]["num_solved"] > 0) {
+            advScore = advancedScore(scoreboard[i]);
+            $('#users').append(
+              "<tr>" +
+              "<td class=\"rank\">" + (i + 1) + "." + "</td>" +
+              "<td class=\"user\">" + user["name"] + "</td>" +
+              "<td class=\"score\">" + Math.round(advScore[0]) + "</td>" +
+              "<td class=\"accuracy\">" + Math.round((advScore[1] / advScore[2]) * 100) + "%</td>" +
+              "<td class=\"user\">" + advScore[1] + "/" + problems.length + "</td>" +
+              "</tr>"
+            );
+          }
+        }
+      });
   });
 });
