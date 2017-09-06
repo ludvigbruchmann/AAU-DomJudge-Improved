79b8f4a9ad217939c8c84576473b3d7ef85098fd
Ludvig Alexander Brüchmann <info@ludvig.xyz>
2017-09-06 14:28:51 +0200

    Version 1

---
+++ index.php
@@ -0,0 +1,51 @@
+<head>
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/motus.min.css">
+  <link rel="stylesheet" href="css/style.min.css">
+
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+  <script src='js/dji.js'></script>
+
+  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" type="text/css">
+
+  <link rel="icon" type="image/png" href="http://img.ludvig.xyz/favicon/favicon.ico">
+  <link rel="apple-touch-icon" sizes="57x57" href="http://img.ludvig.xyz/favicon/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="http://img.ludvig.xyz/favicon/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="http://img.ludvig.xyz/favicon/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="http://img.ludvig.xyz/favicon/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="http://img.ludvig.xyz/favicon/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="http://img.ludvig.xyz/favicon/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="http://img.ludvig.xyz/favicon/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="http://img.ludvig.xyz/favicon/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="http://img.ludvig.xyz/favicon/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="http://img.ludvig.xyz/favicon/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="http://img.ludvig.xyz/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="http://img.ludvig.xyz/favicon/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="http://img.ludvig.xyz/favicon/favicon-16x16.png">
+
+  <title>DomJudge Improved - AAU</title>
+
+</head>
+<body>
+
+  <div class="container">
+
+    <div class="grid-12 center">
+      <h1>DomJudge Improved</h1>
+    </div>
+
+    <div class="grid-6 offset-3">
+      <p class="justify">Only users who have solved at least 1 problem are shown. The score is based on the points given for each problem and how long it took to solve the problem.</p>
+    </div>
+
+    <div class="grid-12 table">
+      <p class="center">Loading...</p>
+    </div>
+
+  </div>
+
+</body>
---
+++ js/dji.js
@@ -0,0 +1,69 @@
+const domjudgeSite  = "get.php";
+const scoreboardUrl = domjudgeSite + "?data=scoreboard";
+const teamsUrl      = domjudgeSite + "?data=teams";
+
+function findUser(array, id) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]["id"] == id){
+      return array[i];
+    }
+  }
+}
+
+function score(user) {
+  if(user["score"]["num_solved"] > 0){
+    return user["score"]["num_solved"] / (user["score"]["total_time"] / 500000);
+  }
+  else {
+    return 0;
+  }
+}
+
+function advancedScore(user) {
+  if(user["score"]["num_solved"] > 0){
+    score = 0;
+    attempts = 0;
+    succeses = 0;
+    for (var i = 0; i < user["problems"].length; i++) {
+      if (user["problems"][i]["solved"]) {
+        score += 1 / (user["problems"][i]["time"] / 500000);
+        attempts += user["problems"][i]["num_judged"]
+        succeses += 1;
+      }
+    }
+    return [score, (succeses / attempts)];
+  }
+  else {
+    return 0;
+  }
+}
+
+$.get( scoreboardUrl, function( data ) {
+  scoreboard = JSON.parse(data);
+  $.get( teamsUrl, function( data ) {
+    teams = JSON.parse(data);
+    teams.sort(function(a, b) {
+      return parseFloat(a.id) - parseFloat(b.id);
+    });
+    $('.table').html(
+      "<table id=\"users\" style=\"width: 100%\" class=\"DomJudgeTable\">" +
+      "<tr><th>#</th><th>User</th><th>Score</th><th>Accuracy</th><th>DomJudge Points</th></tr>" +
+      "</table>"
+    );
+    for (var i = 0; i < scoreboard.length; i++) {
+      user = findUser(teams, scoreboard[i]["team"]);
+      if(scoreboard[i]["score"]["num_solved"] > 0) {
+        advScore = advancedScore(scoreboard[i]);
+        $('#users').append(
+          "<tr>" +
+          "<td class=\"rank\">" + (i + 1) + "." + "</td>" +
+          "<td class=\"user\">" + user["name"] + "</td>" +
+          "<td class=\"score\">" + Math.round(advScore[0]) + "</td>" +
+          "<td class=\"accuracy\">" + Math.round(advScore[1] * 100) + "%</td>" +
+          "<td class=\"user\">" + scoreboard[i]["score"]["num_solved"] + "</td>" +
+          "</tr>"
+        );
+      }
+    }
+  });
+});
